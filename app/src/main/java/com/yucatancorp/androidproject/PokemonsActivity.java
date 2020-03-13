43c361fdd0d6f07aff3f1fe0302e39fffbf1ba9c
marcosArmando <narvaezaldana@gmail.com>
2020-03-12 21:29:19 -0600

    creating recyclerview and creating a variable for it in the java class.

--- app/src/main/java/com/yucatancorp/androidproject/PokemonsActivity.java
+++ app/src/main/java/com/yucatancorp/androidproject/PokemonsActivity.java
@@ -1,6 +1,7 @@
 package com.yucatancorp.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,10 +13,14 @@ public class PokemonsActivity extends AppCompatActivity {
     SharedPreferences sharedPreferencesStatus;
     SharedPreferencesActions sharedPreferencesActions;
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemons);
+
+        recyclerView = findViewById(R.id.recyclerView);
 
         sharedPreferencesStatus = getApplicationContext().getSharedPreferences(STATUS, 0);
 
