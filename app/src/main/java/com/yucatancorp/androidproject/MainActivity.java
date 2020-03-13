f9f1f5f07aa6911e4c8525d300c4ab6332b92853
marcosArmando <narvaezaldana@gmail.com>
2020-03-12 18:32:16 -0600

    checkFields and values implemented and asigned to boolean variables.

--- app/src/main/java/com/yucatancorp/androidproject/MainActivity.java
+++ app/src/main/java/com/yucatancorp/androidproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.yucatancorp.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,11 +22,35 @@ public class MainActivity extends AppCompatActivity {
         passwordUsuario = findViewById(R.id.ETpassword);
         logIn = findViewById(R.id.btnLogIn);
 
+        boolean checarEmail = checkFields(nombreUsuario);
+        boolean checarPassword = checkFields(passwordUsuario);
+
 
     }
 
-    public void checkFields(View v){
+    public boolean checkFields(View v){
 
+        EditText temp = (EditText) v;
+        String tempString;
+
+        tempString = String.valueOf(temp.getText());
+
+        if (tempString.isEmpty()) {
+            temp.setError(getResources().getString(R.string.campoVacio));
+            temp.requestFocus();
+
+            return false;
+        }
+
+        if (temp.getId()== R.id.ETnombre && !Patterns.EMAIL_ADDRESS.matcher(tempString).matches()){
+
+            temp.setError(getResources().getString(R.string.ingreseMail));
+            temp.requestFocus();
+
+            return false;
+        }
+
+        return true;
     }
 
 }
