65f6709f89e9d84fa8249764df3f775be5f0d302
marcosArmando <narvaezaldana@gmail.com>
2020-03-12 21:44:28 -0600

    working on PokemonAdapter and Pokemon POJO

---
+++ app/src/main/java/com/yucatancorp/androidproject/Pokemon.java
@@ -0,0 +1,28 @@
+package com.yucatancorp.androidproject;
+
+public class Pokemon {
+
+    private int fotoPokemon;
+    private String nombrePokemon;
+
+    public Pokemon(int fotoPokemon, String nombrePokemon){
+        this.fotoPokemon = fotoPokemon;
+        this.nombrePokemon = nombrePokemon;
+    }
+
+    public int getFotoPokemon() {
+        return fotoPokemon;
+    }
+
+    public void setFotoPokemon(int fotoPokemon) {
+        this.fotoPokemon = fotoPokemon;
+    }
+
+    public String getNombrePokemon() {
+        return nombrePokemon;
+    }
+
+    public void setNombrePokemon(String nombrePokemon) {
+        this.nombrePokemon = nombrePokemon;
+    }
+}
--- app/src/main/java/com/yucatancorp/androidproject/PokemonAdaptador.java
+++ app/src/main/java/com/yucatancorp/androidproject/PokemonAdaptador.java
@@ -1,14 +1,31 @@
 package com.yucatancorp.androidproject;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PokemonAdaptador {
+public class PokemonAdaptador extends RecyclerView.Adapter<PokemonAdaptador.PokemonViewHolder>{
 
+
+    @NonNull
+    @Override
+    public PokemonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PokemonViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public static class PokemonViewHolder extends RecyclerView.ViewHolder{
 
@@ -17,6 +34,9 @@ public class PokemonAdaptador {
 
         public PokemonViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageView = itemView.findViewById(R.id.imagenCV);
+            textView = itemView.findViewById(R.id.nombrePokemonCV);
         }
     }
 }
