48107108039304d0b1fc620ea300181f6f9469c0
marcosArmando <narvaezaldana@gmail.com>
2020-03-12 20:25:32 -0600

    changing status when login

--- app/src/main/java/com/yucatancorp/androidproject/MainActivity.java
+++ app/src/main/java/com/yucatancorp/androidproject/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
