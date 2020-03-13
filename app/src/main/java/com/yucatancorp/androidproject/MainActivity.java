9c8f4846420b24666dc4934623f848e4a0abf31e
marcosArmando <narvaezaldana@gmail.com>
2020-03-12 21:25:40 -0600

    Implementing recyclerview and cardView libraries at gradle file.

--- app/src/main/java/com/yucatancorp/androidproject/MainActivity.java
+++ app/src/main/java/com/yucatancorp/androidproject/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
