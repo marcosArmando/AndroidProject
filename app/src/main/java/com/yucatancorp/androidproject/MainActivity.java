906697de67a4a9618594f7d595edb39b528fa8f3
marcosArmando <narvaezaldana@gmail.com>
2020-03-12 18:07:56 -0600

    EditTexts and button assigned to variables in java file.

--- app/src/main/java/com/yucatancorp/androidproject/MainActivity.java
+++ app/src/main/java/com/yucatancorp/androidproject/MainActivity.java
@@ -3,12 +3,22 @@ package com.yucatancorp.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nombreUsuario, passwordUsuario;
+    Button logIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombreUsuario = findViewById(R.id.ETnombre);
+        passwordUsuario = findViewById(R.id.ETpassword);
+        logIn = findViewById(R.id.btnLogIn);
+
     }
 }
