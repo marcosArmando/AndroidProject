13d3b69f8ffeb1e4d5b9b2b724c831f08743da61
marcosArmando <narvaezaldana@gmail.com>
2020-03-12 21:34:48 -0600

    creating pokemon adapter and pokemonViewHolder

---
+++ app/src/main/java/com/yucatancorp/androidproject/PokemonAdaptador.java
@@ -0,0 +1,22 @@
+package com.yucatancorp.androidproject;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PokemonAdaptador {
+
+
+    public static class PokemonViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView imageView;
+        public TextView textView;
+
+        public PokemonViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
