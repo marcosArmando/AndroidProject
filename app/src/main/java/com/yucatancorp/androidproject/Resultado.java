a3ba06da614ce8cddab853fdf6fe4d05d9477cef
marcosArmando <narvaezaldana@gmail.com>
2020-03-15 09:18:59 -0600

    Creating resultado POJO

---
+++ app/src/main/java/com/yucatancorp/androidproject/Resultado.java
@@ -0,0 +1,43 @@
+package com.yucatancorp.androidproject;
+
+import java.util.ArrayList;
+
+public class Resultado {
+
+    private int count;
+    private String next;
+    private String previous;
+    private ArrayList<Pokemon> results;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public ArrayList<Pokemon> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<Pokemon> results) {
+        this.results = results;
+    }
+}
